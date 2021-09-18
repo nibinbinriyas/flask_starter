daae1b1d5bdd5cfec0f546c3b93eb309c636c24f
nibinbinriyas <nibinbinriyas@gmail.com>
2021-09-19 01:10:34 +0530

    modified

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/registration")
+def registration():
+    return render_template("register.html")
+
+@app.route('/courses')
+def course():
+    return render_template("course.html")
+
+@app.route('/courses/course1')
+def course1():
+    return render_template('course1.html')
+
+@app.route('/courses/course2')
+def course2():
+    return render_template('course2.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
